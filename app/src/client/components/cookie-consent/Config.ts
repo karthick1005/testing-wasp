6d1dcc7fa3d8983af8daf5892ef13d5fab406217
karthick1005 <73534542+karthick1005@users.noreply.github.com>
2025-06-20 09:07:52 +0530

    Update Config.ts

--- app/src/client/components/cookie-consent/Config.ts
+++ app/src/client/components/cookie-consent/Config.ts
@@ -58,6 +58,7 @@ const getConfig = () => {
             label: 'Google Analytics',
             onAccept: () => {
   try {
+    console.log('GA ID:', import.meta.env.REACT_APP_GOOGLE_ANALYTICS_ID);
     const GA_ANALYTICS_ID = import.meta.env.REACT_APP_GOOGLE_ANALYTICS_ID;
     if (!GA_ANALYTICS_ID?.length) {
       throw new Error('Google Analytics ID is missing');
