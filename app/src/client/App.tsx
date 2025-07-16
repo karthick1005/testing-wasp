f86110acedc092d2c29c432833b3d26ca544277d
karthick1005 <udhayakarthick.mnm4@gmail.com>
2025-07-16 21:22:29 +0530

    changes new

--- app/src/client/App.tsx
+++ app/src/client/App.tsx
@@ -67,6 +67,7 @@ useEffect(() => {
         window.gtag("event", "pwa_installed", {
           event_category: "PWA",
           event_label: navigator.platform,
+           debug_mode: true, // 👈 This enables GA4 DebugView manually
         });
       } catch (error) {
         console.error("Error sending GA4 event for PWA installation:", error);
