f14f57c1f735c8ef62287d676ffaefa2fb4c4e49
karthick1005 <udhayakarthick.mnm4@gmail.com>
2025-08-02 10:38:28 +0530

    new changes

--- template/app/vite.config.ts
+++ template/app/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-})
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'], // assets to cache
+      manifest: {
+        name: 'Anxious Love',
+        short_name: 'Eli',
+        description: 'Eli is a virtual friend for your anxiety.',
+        start_url: '/',
+        display: 'standalone',
+        scope: '/',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
